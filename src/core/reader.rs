5709c4b2327b3c1166adc8e4d5fa463c6688dd13
Jérémie <jeremie.jourda202@gmail.com>
2024-01-08 00:12:42 +0100

    draft: big refactor

--- src/core/reader.rs
+++ src/core/reader.rs
@@ -3,7 +3,9 @@ use std::ffi::c_void;
 use std::fmt::Debug;
 use std::mem::size_of;
 use std::str::from_utf8;
-use std::{any::type_name};
+use std::sync::OnceLock;
+use std::sync::{Arc, Mutex};
+use std::{any::type_name, ops::Add};
 
 use toy_arms::external::module::Module;
 use toy_arms::external::process::Process;
@@ -16,6 +18,80 @@ use crate::structs::tarray::TArray;
 const UWORLDPATTERN: &'static str = "48 8B 05 ? ? ? ? 48 8B 88 ? ? ? ? 48 85 C9 74 06 48 8B 49 70";
 const GOBJECTPATTERN: &'static str = "89 0D ? ? ? ? 48 8B DF 48 89 5C 24";
 const GNAMEPATTERN: &'static str = "48 8B 1D ? ? ? ? 48 85 DB 75 ? B9 08 04 00 00";
+
+struct MyProcess {
+    pub name: &'static str,
+    pub id: u32,
+    pub handle: usize,
+}
+
+fn process() -> &'static MyProcess {
+    static PROCESS: OnceLock<MyProcess> = OnceLock::new();
+    PROCESS.get_or_init(|| {
+        let process = Process::from_process_name("SoTGame.exe").unwrap();
+        MyProcess {
+            name: process.name,
+            id: process.id,
+            handle: process.handle as usize,
+        }
+    })
+}
+
+pub fn read_pointer<T>(address: *mut T) -> T {
+    let mut target_buffer: T = unsafe { std::mem::zeroed() };
+    let _bytes_read = 0;
+    read::<T>(
+        &(process().handle as *mut c_void),
+        address as usize,
+        size_of::<T>(),
+        &mut target_buffer as *mut T,
+    )
+    .unwrap();
+    target_buffer
+}
+
+pub fn read_bytes(address: usize, size: usize) -> Vec<u8> {
+    let mut target_buffer: Vec<u8> = vec![0; size];
+    read::<u8>(
+        &(process().handle as *mut c_void),
+        address,
+        size,
+        target_buffer.as_mut_ptr(),
+    )
+    .unwrap();
+    target_buffer
+}
+
+pub fn read_array<T>(address: usize) -> TArray<T> {
+    let buffer = read_bytes(address, 12);
+
+    let mut base_address_bytes = [0; 8];
+    base_address_bytes.copy_from_slice(&buffer[0..8]);
+    let base_address: u64 = u64::from_le_bytes(base_address_bytes);
+
+    let mut count_bytes = [0; 4];
+    count_bytes.copy_from_slice(&buffer[8..12]);
+    let count: u32 = u32::from_le_bytes(count_bytes);
+
+    let item_size = size_of::<T>();
+    let raw_bytes = read_bytes(base_address as usize, item_size * count as usize);
+
+    TArray::new(raw_bytes, count)
+}
+
+pub fn find_dma_addy<T>(address: usize, mut offsets: Vec<u32>) -> T {
+    if offsets.is_empty() {
+        panic!("Offsets vector is empty. Expected at least one element.");
+    }
+    let mut current_address = address;
+    let last_offset = offsets.pop().unwrap();
+
+    for offset in offsets {
+        current_address = current_address + offset as usize;
+        current_address = read_pointer(current_address as *mut _);
+    }
+    read_pointer::<T>((current_address + last_offset as usize) as *mut T)
+}
 
 #[derive(Debug)]
 pub enum MemoryReaderError {
@@ -32,8 +108,6 @@ pub struct ActorInfo {
 }
 
 pub struct MemoryReader {
-    process: Process<'static>,
-    module: Module,
     u_world_base: usize,
     g_object_base: usize,
     g_name_base: usize,
@@ -41,9 +115,9 @@ pub struct MemoryReader {
 }
 
 impl MemoryReader {
-    pub fn new(process_name: &'static str) -> Result<Self, &'static str> {
-        let process = Process::from_process_name(process_name).expect("Process could be found");
-        let mut module = process.get_module_info(process_name).unwrap();
+    pub fn new(process_name: &'static str) -> Self {
+        let process_ = Process::from_process_name("SoTGame.exe").unwrap();
+        let mut module = process_.get_module_info("SoTGame.exe").unwrap();
 
         let u_world_base = module
             .find_pattern(UWORLDPATTERN)
@@ -55,39 +129,23 @@ impl MemoryReader {
             .find_pattern(GNAMEPATTERN)
             .expect("Could not find g_name_base offsets");
 
-        let mut g_name_offset: u32 = 0;
-        read::<u32>(
-            &process.handle,
-            module.base_address + g_name_base + 3,
-            size_of::<u32>(),
-            &mut g_name_offset as *mut u32,
-        )
-        .expect("Could not get g_name_offset offset");
+        let g_name_offset = read_pointer((module.base_address + g_name_base + 3) as *mut u32);
+        let g_name_ptr = module.base_address + g_name_base + (g_name_offset as usize + 7);
 
-        let g_name_ptr: usize = module.base_address + g_name_base + (g_name_offset as usize + 7);
-        let mut g_name_start_address: u64 = 0;
-        read::<u64>(
-            &process.handle,
-            g_name_ptr,
-            size_of::<u64>(),
-            &mut g_name_start_address as *mut u64,
-        )
-        .expect("Could not get g_name_start_address offset");
+        let g_name_start_address = read_pointer(g_name_ptr as *mut u64);
 
-        Ok(Self {
-            process,
-            module,
+        Self {
             u_world_base,
             g_object_base,
             g_name_base,
             g_name_start_address,
-        })
+        }
     }
 
     pub fn check_process_is_active(&self) -> bool {
         let s = System::new_all();
-        for process in s.processes_by_exact_name(self.process.name) {
-            if process.pid().as_u32() == self.process.id {
+        for process_ in s.processes_by_exact_name(process().name) {
+            if process_.pid().as_u32() == process().id {
                 return true;
             }
         }
@@ -99,12 +157,7 @@ impl MemoryReader {
     }
 
     pub fn read_string(&self, address: usize, size: usize) -> Result<String, MemoryReaderError> {
-        let buffer = self.read_bytes(address, size).map_err(|_err| {
-            MemoryReaderError::MemoryReadingError(format!(
-                "Could not read string at {:#X}",
-                address
-            ))
-        })?;
+        let buffer = read_bytes(address, size);
 
         let i = match buffer.iter().position(|r| r == &b'\x00') {
             Some(v) => v,
@@ -124,19 +177,7 @@ impl MemoryReader {
         address: usize,
         size: usize,
     ) -> Result<String, MemoryReaderError> {
-        let mut target_buffer: Vec<u8> = vec![];
-        read::<Vec<u8>>(
-            &self.process.handle,
-            address,
-            size,
-            &mut target_buffer as *mut Vec<u8>,
-        )
-        .map_err(|_err| {
-            MemoryReaderError::MemoryReadingError(format!(
-                "Could not read string at {:#X}",
-                address
-            ))
-        })?;
+        let target_buffer = read_bytes(address, size);
 
         let i = target_buffer
             .windows(3)
@@ -158,43 +199,9 @@ impl MemoryReader {
     pub fn read_gname(&self, actor_id: u32) -> Result<String, MemoryReaderError> {
         let actor_id = u64::from(actor_id);
         let name_ptr =
-            self.read_pointer((self.g_name_start_address + actor_id / 0x4000 * 0x8) as *mut u64)?;
-        let name = self.read_pointer((name_ptr + 0x8 * (actor_id % 0x4000)) as *mut u64)?;
+            read_pointer((self.g_name_start_address + actor_id / 0x4000 * 0x8) as *mut u64);
+        let name = read_pointer((name_ptr + 0x8 * (actor_id % 0x4000)) as *mut u64);
         Ok(self.read_string((name + 0x10) as usize, 64))?
-    }
-
-    pub fn read_pointer<T>(&self, address: *mut T) -> Result<T, MemoryReaderError> {
-        // TODO: rename
-        let mut buffer: T = unsafe { std::mem::zeroed() };
-        let _bytes_read = 0;
-        read::<T>(
-            &self.process.handle,
-            address as usize,
-            size_of::<T>(),
-            &mut buffer as *mut T as _,
-        )
-        .map_err(|_err| {
-            MemoryReaderError::MemoryReadingError(format!(
-                "Could not read {} type at {:#X}",
-                type_name::<T>(),
-                address as usize
-            ))
-        })?;
-        Ok(buffer)
-    }
-
-    pub fn read_bytes(&self, address: usize, size: usize) -> Result<Vec<u8>, MemoryReaderError> {
-        let mut target_buffer: Vec<u8> = vec![0; size];
-        read::<u8>(
-            &self.process.handle,
-            address,
-            size,
-            target_buffer.as_mut_ptr(),
-        )
-        .map_err(|_err| {
-            MemoryReaderError::MemoryReadingError(format!("Could not read bytes at {:#X}", address))
-        })?;
-        Ok(target_buffer)
     }
 }
 
@@ -205,51 +212,31 @@ pub struct SoTMemoryReader {
 
 impl SoTMemoryReader {
     pub fn new(process_name: &'static str) -> Result<Self, MemoryReaderError> {
-        let rm = MemoryReader::new(process_name)
-            .map_err(|_err| MemoryReaderError::InitializationError(String::from(_err)))?;
-        let base_address = rm.module.base_address;
+        let rm = MemoryReader::new(process_name);
+        let process_ = Process::from_process_name("SoTGame.exe").unwrap();
+        let mut module = process_.get_module_info("SoTGame.exe").unwrap();
+        let base_address = module.base_address;
 
         let u_world_offset =
-            rm.read_pointer::<u32>((base_address + rm.u_world_base + 3) as *mut u32)? as usize;
+            read_pointer::<u32>((base_address + rm.u_world_base + 3) as *mut u32) as usize;
         let u_world_ptr = (base_address + rm.u_world_base + u_world_offset + 7) as *mut u64;
-        let world_address = rm.read_pointer::<u64>(u_world_ptr)? as usize;
+        let world_address = read_pointer::<u64>(u_world_ptr) as usize;
         let _g_objects_offset =
-            rm.read_pointer::<u64>((base_address + rm.g_object_base + 2) as *mut u64)? as usize;
+            read_pointer::<u64>((base_address + rm.g_object_base + 2) as *mut u64) as usize;
         let _g_objects_address = base_address + rm.g_object_base + _g_objects_offset + 22;
 
         Ok(Self { rm, world_address })
-    }
-
-    pub fn read_array<T>(&self, address: usize) -> TArray<T> {
-        let buffer = self.rm.read_bytes(address, 12).unwrap();
-
-        let mut base_address_bytes = [0; 8];
-        base_address_bytes.copy_from_slice(&buffer[0..8]);
-        let base_address: u64 = u64::from_le_bytes(base_address_bytes);
-
-        let mut count_bytes = [0; 4];
-        count_bytes.copy_from_slice(&buffer[8..12]);
-        let count: u32 = u32::from_le_bytes(count_bytes);
-
-        let item_size = size_of::<T>();
-        let raw_bytes = self
-            .rm
-            .read_bytes(base_address as usize, item_size * count as usize)
-            .unwrap();
-
-        TArray::new(raw_bytes, count)
     }
 
     pub fn read_actors(
         &mut self,
         actor_name_map: &mut HashMap<u32, ActorInfo>,
     ) -> Result<(), MemoryReaderError> {
-        let levels_pointer_table =
-            self.read_array::<*mut c_void>((self.world_address + 0x150) as usize);
+        let levels_pointer_table = read_array::<*mut c_void>((self.world_address + 0x150) as usize);
 
         for level_base_address in levels_pointer_table.iter() {
             let actors_pointer_table =
-                self.read_array::<*mut c_void>(level_base_address as usize + 0xa0);
+                read_array::<*mut c_void>(level_base_address as usize + 0xa0);
 
             if actors_pointer_table.count == 0 {
                 println!("This level has no actors");
@@ -257,26 +244,22 @@ impl SoTMemoryReader {
             }
 
             for actor_base_address in actors_pointer_table.iter() {
-                if let Ok(actor_id) = self
-                    .rm
-                    .read_pointer::<u32>((actor_base_address as usize + 0x18) as *mut u32)
-                {
-                    let _ = if let Some(_) = actor_name_map.get(&actor_id) {
-                        continue;
-                    } else {
-                        let name = match self.rm.read_gname(actor_id) {
-                            Ok(v) => v,
-                            Err(_) => continue,
-                        };
-                        let new_actor_info = ActorInfo {
-                            id: actor_id,
-                            raw_name: name,
-                            base_address: actor_base_address as usize,
-                        };
-                        actor_name_map.insert(actor_id, new_actor_info);
-                        actor_name_map.get(&actor_id).unwrap()
+                let actor_id = read_pointer((actor_base_address as usize + 0x18) as *mut u32);
+                let _ = if let Some(_) = actor_name_map.get(&actor_id) {
+                    continue;
+                } else {
+                    let name = match self.rm.read_gname(actor_id) {
+                        Ok(v) => v,
+                        Err(_) => continue,
                     };
-                }
+                    let new_actor_info = ActorInfo {
+                        id: actor_id,
+                        raw_name: name,
+                        base_address: actor_base_address as usize,
+                    };
+                    actor_name_map.insert(actor_id, new_actor_info);
+                    actor_name_map.get(&actor_id).unwrap()
+                };
             }
         }
         Ok(())
--- src/services/event_loop.rs
+++ src/services/event_loop.rs
@@ -8,7 +8,7 @@ use ggez::event::EventHandler;
 use ggez::graphics::{self, Color};
 use ggez::{Context, GameResult};
 
-use crate::core::reader::{ActorInfo, SoTMemoryReader};
+use crate::core::reader::{find_dma_addy, read_pointer, ActorInfo, SoTMemoryReader};
 use crate::entities::world::World;
 use crate::structs::unreal::UObject;
 
@@ -78,6 +78,12 @@ impl EventHandler for MyGame {
             }
         }
 
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
+        let mut reader = self.sot_memory_reader.lock().unwrap();
         for (table_actor, emissary_label) in vec![
             (&self.world.gold_hoarders_emissary_table, "Gold Hoarders : "),
             (&self.world.merchant_alliance_emissary_table, "Merchants : "),
@@ -93,48 +99,30 @@ impl EventHandler for MyGame {
                 continue;
             }
             let table_actor_info = table_actor.clone().unwrap();
-            let a = reader
-                .rm
-                .read_pointer(table_actor_info.base_address as *mut UObject)
-                .unwrap();
+            let u_object = read_pointer(table_actor_info.base_address as *mut UObject);
 
-            let class_ = reader.rm.read_pointer(a.u_class).unwrap();
-
+            let class_ = read_pointer(u_object.u_class);
             let class_name = reader.rm.read_gname(class_.name.index).unwrap();
 
             let emissary_ship_affiliation_tracker_offset = self
                 .sdk_service
                 .get_offset(&format!("{}.EmissaryShipAffiliationTracker", class_name));
 
-            let emissary_ship_affiliation_tracker_class = reader
-                .rm
-                .read_pointer(
-                    (table_actor_info.base_address
-                        + emissary_ship_affiliation_tracker_offset as usize)
-                        as *mut *mut c_void,
-                )
-                .unwrap();
-
             let emissary_count_offset = self
                 .sdk_service
                 .get_offset("EmissaryShipAffiliationTrackerComponent.EmissaryCount");
 
-            let emmisary_count = reader
-                .rm
-                .read_pointer(
-                    (emissary_ship_affiliation_tracker_class as usize
-                        + emissary_count_offset as usize) as *mut u32,
-                )
-                .unwrap();
+            let emmisary_count = find_dma_addy::<u32>(
+                table_actor_info.base_address,
+                vec![
+                    emissary_ship_affiliation_tracker_offset,
+                    emissary_count_offset,
+                ],
+            );
 
             println!("{} {}", emissary_label, emmisary_count);
         }
 
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
         // Draw code here...
         canvas.finish(ctx)
     }
